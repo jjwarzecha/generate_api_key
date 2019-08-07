25d6a9f655c7adbf5f47635faa5a11197fa5570a
JJ Warzecha <jj.warzecha@gmail.com>
2019-08-07 11:50:07 +0200

    #jj - working setup, generate key and fingerprint

--- main.py
+++ main.py
@@ -2,67 +2,40 @@
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
-import base64
-import hashlib
-import binascii
+from codecs import decode
+from hashlib import md5
 import re
-import sys
-
-__license__ = "MIT"
 
 
-def lineToFingerprint(line):
-    key = base64.b64decode(line.strip().split()[1].encode('ascii'))
-    fp_plain = hashlib.md5(key).hexdigest()
-    return ':'.join(a+b for a,b in zip(fp_plain[::2], fp_plain[1::2]))
 
+def get_fingerprint(public_key):
+    # source: https://github.com/oracle/oci-python-sdk/blob/master/examples/add_API_key.py
+    # There should be more error checking here, but to keep the example simple
+    # the error checking has been omitted.
+    m = md5()
 
-def insert_char_every_n_chars(string, char='\n', every=64):
-    return char.join(
-        string[i:i + every] for i in xrange(0, len(string), every))
+    # Strip out the parts of the key that are not used in the fingerprint
+    # computation.
+    public_key = public_key.replace(b'-----BEGIN PUBLIC KEY-----\n', b'')
+    public_key = public_key.replace(b'\n-----END PUBLIC KEY-----', b'')
 
+    # The key is base64 encoded and needs to be decoded before getting the md5 hash
+    decoded_key = decode(public_key, "base64")
+    m.update(decoded_key)
+    hash = m.hexdigest()
 
-def md5_fingerprint_from_pub_key(data):
-    data = data.strip()
+    length = 2
+    parts = list(hash[0 + i:length + i] for i in range(0, len(hash), length))
+    fingerprint = ":".join(parts)
 
-    # accept either base64 encoded data or full pub key file,
-    # same as `fingerprint_from_ssh_pub_key
-    if (re.search(r'^ssh-(?:rsa|dss) ', data)):
-        data = data.split(None, 2)[1]
-
-    # Python 2/3 hack. May be a better solution but this works.
-    try:
-        data = bytes(data, 'ascii')
-    except TypeError:
-        data = bytes(data)
-
-    md5digest = hashlib.md5(data.exportKey('DER')).hexdigest()
-    fingerprint = insert_char_every_n_chars(md5digest, ':', 2)
     return fingerprint
-
-
-def sha256_fingerprint_from_pub_key(data):
-    data = data.strip()
-
-    # accept either base64 encoded data or full pub key file,
-    # same as `fingerprint_from_ssh_pub_key
-    if (re.search(r'^ssh-(?:rsa|dss) ', data)):
-        data = data.split(None, 2)[1]
-
-    # Python 2/3 hack. May be a better solution but this works.
-    try:
-        data = bytes(data, 'ascii')
-    except TypeError:
-        data = bytes(data)
-
-    digest = hashlib.sha256(binascii.a2b_base64(data)).digest()
-    encoded = base64.b64encode(digest).rstrip(b'=')  # ssh-keygen strips this
-    return "SHA256:" + encoded.decode('utf-8')
 
 
 
 def main():
     """ Main entry point of the app """
+
+    # generate private key
     key = rsa.generate_private_key(public_exponent=65537,
                                    key_size=4096, backend=default_backend())
 
@@ -71,7 +44,7 @@ def main():
                                     format=serialization.PrivateFormat.TraditionalOpenSSL,
                                     encryption_algorithm=serialization.NoEncryption())
 
-    # get public key in OpenSSH format
+    # get public key in PEM format
     pem_public_key = key.public_key().public_bytes(encoding=serialization.Encoding.PEM,
                                                    format=serialization.PublicFormat.SubjectPublicKeyInfo)
 
@@ -86,15 +59,14 @@ def main():
 
     print('Private RSA key in PEM format:')
     print(out_pem_private_key_str)
-    # print(lineToFingerprint(out_pem_private_key_str))
-    #print(md5_fingerprint_from_pub_key(out_pem_private_key_str))
-
+    print('+++++++++++++++++++++++++++++++++++++++++++++')
     print('Public RSA key in PEM format:')
     print(out_pem_public_key_str)
-
+    print('+++++++++++++++++++++++++++++++++++++++++++++')
+    print('Fingerprint:',get_fingerprint(pem_public_key))
+    print('+++++++++++++++++++++++++++++++++++++++++++++')
     print('Public RSA key in OpenSSH format:')
     print(out_ssh_public_key_str)
-    # print(lineToFingerprint(out_ssh_public_key_str))
 
 
 if __name__ == "__main__":
